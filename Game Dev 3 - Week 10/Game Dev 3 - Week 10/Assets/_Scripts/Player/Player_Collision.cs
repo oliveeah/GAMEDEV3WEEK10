9342675fef4c550e29acf31c8faae37511f68fec
oliveeah <olliebennettx@gmail.com>
2025-01-06 21:36:59 +0000

    donew

--- Game Dev 3 - Week 10/Game Dev 3 - Week 10/Assets/_Scripts/Player/Player_Collision.cs
+++ Game Dev 3 - Week 10/Game Dev 3 - Week 10/Assets/_Scripts/Player/Player_Collision.cs
@@ -1,76 +1,23 @@
-﻿using GameDevWithMarco.Managers;
-using GameDevWithMarco.VFX;
-using System.Collections;
-using System.Collections.Generic;
+﻿using GameDevWithMarco.Interfaces;
 using UnityEngine;
 
 namespace GameDevWithMarco.Player
 {
     public class Player_Collision : MonoBehaviour
     {
-        //References
-        private VfxManager vfx;
-        private Ripple ripple;
-        public UIManager ui;
-
-        [SerializeField] GameEvent GoodPackageCollected;
-        [SerializeField] GameEvent BadPackageCollected;
-        [SerializeField] GameEvent LifePackageCollected;
-
-        //Variables
-        public bool greenCollected = false;
-
-        void Start()
-        {
-            Initialisation();
-        }
-
-        public void OnTriggerEnter2D(Collider2D collision)
+        private void OnTriggerEnter2D(Collider2D collision)
         {
             ExecuteLogicBasedOnWhatWeHaveCollidedWith(collision);
             collision.gameObject.SetActive(false);
-            }
+        }
 
         private void ExecuteLogicBasedOnWhatWeHaveCollidedWith(Collider2D collision)
         {
-            switch (collision.gameObject.tag)
-            {
-                case "GoodBox":
-                    GoodPackageCollected.Raise();
-                    GameManager.Instance.GreenPackLogic();
-                    vfx.GoodPickupParticles();
-                    vfx.AddPointsPromptMethod();
-                    ripple.RippleReaction();
-                    AudioManager.Instance.GoodPickupSound();
-                    greenCollected = true;
-                    break;
-                case "BadBox":
-                    BadPackageCollected.Raise();
-                    GameManager.Instance.RedPackLogic();
-                    vfx.CamShake();
-                    vfx.BadPickupParticles();
-                    vfx.SubtractPointsPromptMethod();
-                    ui.MinusOneLifeFeedback();
-                    AudioManager.Instance.BadPickupSound();
-                    break;
-                case "LifeBox":
-                    LifePackageCollected.Raise();
-                    GameManager.Instance.lives++;
-                    ripple.RippleReaction();
-                    ui.PlusOneLifeFeedback();
-                    AudioManager.Instance.LifePickupSound();
-                    break;
-                default:
-                    break;
-            }
+            ICollidable collidable = collision.GetComponent<ICollidable>();
+            if(collidable != null) { collidable.CollidedLogic(); }
         }
 
-        private void Initialisation()
-        {
-            vfx = FindObjectOfType<VfxManager>();
-            ripple = FindObjectOfType<Ripple>();
-            ui = FindObjectOfType<UIManager>();
-        }
+
     }
 
 }
